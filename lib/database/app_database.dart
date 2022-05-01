e38ab96ce3d8e9b6c016184a5275cb00d2e22a31
Diego Rodrigues <diegokod@gmail.com>
2022-05-01 12:21:51 -0300

    db configs add

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,14 +1,39 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-void createDataBase() {
-  getDatabasesPath().then((dbPath) {
+import '../models/contact.dart';
+
+Future<Database> createDataBase() {
+  return getDatabasesPath().then((dbPath) {
     final String path = join(dbPath, 'bytebank.db');
-    openDatabase(path, onCreate: (db, version) {
+    return openDatabase(path, onCreate: (db, version) {
       db.execute('CREATE TABLE contacts('
           'id INTEGER PRIMARY KEY,'
           'name TEXT,'
           'account_number INTEGER)');
     }, version: 1);
+  });
+}
+
+Future<int> save(Contact contact) {
+  return createDataBase().then((db) {
+    final Map<String, dynamic> contactMap = Map();
+    contactMap['name'] = contact.name;
+    contactMap['account_number'] = contact.accountNumber;
+    return db.insert('contacts', contactMap);
+  });
+}
+
+Future<List<Contact>> findAll() {
+  return createDataBase().then((db) {
+    return db.query('contacts').then((maps) {
+      final List<Contact> contacts = [];
+      for (Map<String, dynamic> map in maps) {
+        final Contact contact =
+            Contact(map['id'], map['name'], map['account_number']);
+        contacts.add(contact);
+      }
+      return contacts;
+    });
   });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'database/app_database.dart';
+import 'models/contact.dart';
 import 'screens/contact_form.dart';
 import 'screens/contacts_list.dart';
 import 'screens/dashboard.dart';
 
 void main() {
   runApp(const ByteBankApp());
+  save(Contact(0, 'Diego', 1000)).then((id) {
+    findAll().then((contacts) => debugPrint(contacts.toString()));
+  });
 }
 
 class ByteBankApp extends StatelessWidget {
