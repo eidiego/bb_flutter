006a37f72d663242265235d5fcd942b4666cd59e
Diego Rodrigues <diegokod@gmail.com>
2022-05-02 20:48:10 -0300

    Fix: API config

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart';
 
 void findAll() async {
   final Response response = await get(
-    Uri.parse('http://localhost:8080/transactions'),
+    Uri.parse('http://192.168.1.40:8080/transactions'),
   );
   print(response.body);
 }
