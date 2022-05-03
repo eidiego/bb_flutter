f856a5cdb6288206fce1d13a1bcb79c24a0c1505
Diego Rodrigues <diegokod@gmail.com>
2022-05-02 23:34:38 -0300

    Add: CenteredMEssages component

--- lib/screens/transaction_list.dart
+++ lib/screens/transaction_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../components/centered_message.dart';
 import '../components/progress.dart';
 import '../http/webclient.dart';
 import '../models/transaction.dart';
@@ -19,7 +20,6 @@ class TransactionsList extends StatelessWidget {
           builder: (context, snapshot) {
             switch (snapshot.connectionState) {
               case ConnectionState.none:
-                // TODO: Handle this case.
                 break;
               case ConnectionState.waiting:
                 return Progress(message: 'Carregando transações');
@@ -30,33 +30,39 @@ class TransactionsList extends StatelessWidget {
               case ConnectionState.done:
                 final List<Transaction> transactions =
                     snapshot.data as List<Transaction>;
-                return ListView.builder(
-                  itemBuilder: (context, index) {
-                    final Transaction transaction = transactions[index];
-                    return Card(
-                      child: ListTile(
-                        leading: Icon(Icons.monetization_on),
-                        title: Text(
-                          transaction.value.toString(),
-                          style: const TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
+                if (transactions.isNotEmpty) {
+                  return ListView.builder(
+                    itemBuilder: (context, index) {
+                      final Transaction transaction = transactions[index];
+                      return Card(
+                        child: ListTile(
+                          leading: Icon(Icons.monetization_on),
+                          title: Text(
+                            transaction.value.toString(),
+                            style: const TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            transaction.contact.accountNumber.toString(),
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                            ),
                           ),
                         ),
-                        subtitle: Text(
-                          transaction.contact.accountNumber.toString(),
-                          style: const TextStyle(
-                            fontSize: 16.0,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                  itemCount: transactions.length,
+                      );
+                    },
+                    itemCount: transactions.length,
+                  );
+                }
+                return CenteredMessage(
+                  'No transactions found',
+                  icon: Icons.warning,
                 );
                 break;
             }
-            return Text('Unknown error');
+            return CenteredMessage('Unknown error');
           },
         ),
       ),
