0ea74099071c2316c228af3a20ac7b2e8c7ec6ae
Diego Rodrigues <diegokod@gmail.com>
2022-05-02 23:51:36 -0300

    Refactor: add timeout

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -31,9 +31,11 @@ class LoggingInterceptor implements InterceptorContract {
 Future<List<Transaction>> findAll() async {
   final Client client =
       InterceptedClient.build(interceptors: [LoggingInterceptor()]);
-  final Response response = await client.get(
-    Uri.parse('http://192.168.1.40:8080/transactions'),
-  );
+  final Response response = await client
+      .get(
+        Uri.parse('http://192.168.1.40:8080/transactions'),
+      )
+      .timeout(const Duration(seconds: 5));
   final List<dynamic> decodedJson = jsonDecode(response.body);
   final List<Transaction> transactions = [];
   for (Map<String, dynamic> transactionJson in decodedJson) {
