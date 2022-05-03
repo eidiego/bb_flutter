c5fa8e05adc6c00a718509aa44cc0f8aa61dd867
Diego Rodrigues <diegokod@gmail.com>
2022-05-02 21:13:11 -0300

    Refactor: Webclient.dart

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -7,13 +7,16 @@ class LoggingInterceptor implements InterceptorContract {
   @override
   Future<RequestData> interceptRequest({required RequestData data}) async {
     print(data.toString());
+    print('url: ${data.url.toString()}');
+    print('headers ${data.headers.toString()}');
+    print('body ${data.body.toString()}');
     return data;
   }
 
   @override
   Future<ResponseData> interceptResponse({required ResponseData data}) async {
     // ignore: avoid_print
-    print('url: ${data.url.toString()}');
+    print('status code: ${data.statusCode.toString()}');
     print('headers ${data.headers.toString()}');
     print('body ${data.body.toString()}');
     return data;
